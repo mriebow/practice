f90820e56c380fbaffd4ffc7558de4ccaf327df9
matthewriebow@gmail.com <matthewriebow@gmail.com>
2017-06-12 12:18:04 -0400

    Learning PHP

---
+++ PHP/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	</head>
+	<body>
+	    <h1>
+	       <?php
+	        echo "This "."is "."a "."concatenated "."string.";
+	       ?>
+	    </h1>
+        <p>
+          <?php
+            echo 17 * 123
+          ?>
+        </p>
+	</body>
+</html>
