e957270f808ea2ff7cbd777585fe9e20753e34e1
matthewriebow@gmail.com <matthewriebow@gmail.com>
2017-06-22 12:34:25 -0400

    Animate name

---
+++ animate-name/main.js
@@ -0,0 +1,17 @@
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+
+var myName = "Matthew";
+letterColors = [red, orange, green, blue, purple];
+if (2 == 2){
+    bubbleShape = "circle";
+    }
+else{
+    bubbleShape = "square";
+}
+
+drawName(myName, letterColors);
+bounceBubbles();
