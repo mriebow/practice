248876b32a079fa1150bfe27e439fc690cf7d949
matthewriebow@gmail.com <matthewriebow@gmail.com>
2017-06-27 12:57:53 -0400

    PHP Functions II

--- PHP/index.php
+++ PHP/index.php
@@ -112,3 +112,11 @@ echo "<p>It took {$flipCount} flips!</p>";
 ?>
 	</body>
 </html>
+<!-- functions II -->
+<?php
+function aboutMe($name, $age){
+		echo "Hello! My name is ".$name.", and I am ".$age." years old.";
+		}
+
+		aboutMe("Matthew",31)
+?>
