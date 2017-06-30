5c5c18ffd93def10284abef4106cfffb5246ed50
matthewriebow@gmail.com <matthewriebow@gmail.com>
2017-06-30 15:50:34 -0400

    codecademy react

---
+++ react/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Write code here:
+const myList = (
+<ul>
+  <li>Get a job</li>
+  <li>Get really good at a job</li>
+</ul>
+);
+
+ReactDOM.render(
+  myList,
+  document.getElementById('app')
+);
