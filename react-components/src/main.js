33b2042e0f9174acebe88561cfb71ada0318051c
matthewriebow@gmail.com <matthewriebow@gmail.com>
2017-07-07 14:46:31 -0400

    Remove components, work on nested components

--- react-components/src/main.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MessageComponent from './components/MessageComponent';
-
-ReactDOM.render(
-  <MessageComponent message="Hello from main.js"  />,
-  document.getElementById('app')
-);
