b58c74d328e26c0367e0a839cc8669b6ad843290
matthewriebow@gmail.com <matthewriebow@gmail.com>
2017-07-09 14:39:56 -0400

    Launchcode practice test

---
+++ launchcode/countup.rb
@@ -0,0 +1,8 @@
+
+def  countUp(start)
+  puts "#{start + 1} then #{start + 2} then #{start + 3} then #{start + 4} then #{start + 5} then #{start + 6} then #{start + 7} then #{start + 8} then #{start + 9} then #{start + 10}"
+end
+
+_start = Integer(gets);
+
+countUp(_start);
