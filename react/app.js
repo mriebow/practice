6baad90dc3fd79dfa397e739920eaedc4815b7fd
matthewriebow@gmail.com <matthewriebow@gmail.com>
2017-07-17 20:32:06 -0400

    Learning Components

--- react/app.js
+++ react/app.js
@@ -72,3 +72,18 @@ const greatestDivEver = React.createElement(
   null,
   "i am div"
 );
+
+// Components
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MyComponentClass extends React.Component {
+  render() {
+    return <h1>Hello world</h1>;
+  }
+}
+
+ReactDOM.render(
+	<MyComponentClass />,
+	document.getElementById('app')
+);
